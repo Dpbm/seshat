500a59255a95445aa6ab23de79823da4972f6e39
Dpbm <dpbm136@gmail.com>
2024-05-07 15:53:35 -0300

    fixed flutter version

--- dep-ubuntu.sh
+++ dep-ubuntu.sh
@@ -3,4 +3,3 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y libsqlite3-0 libsqlite3-dev build-essential jq wget curl binutils git ninja-build libgtk-3-dev
-sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
