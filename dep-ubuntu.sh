475f3ea8f7738c93ce910a7ace5679b074ed7a0e
Dpbm <dpbm136@gmail.com>
2024-05-07 15:46:04 -0300

    updated actions

--- dep-ubuntu.sh
+++ dep-ubuntu.sh
@@ -2,4 +2,5 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y libsqlite3-0 libsqlite3-dev build-essential jq wget curl binutils git
+sudo apt install -y libsqlite3-0 libsqlite3-dev build-essential jq wget curl binutils git ninja-build libgtk-3-dev
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
