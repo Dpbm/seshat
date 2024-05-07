9e20e340af84db9c1f4bec437bc80f9e91015b86
Dpbm <dpbm136@gmail.com>
2024-05-07 15:41:19 -0300

    fixed dep-ubuntu script

--- dep-ubuntu.sh
+++ dep-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt install -y libsqlite3-0 libsqlite3-dev build-essential jq wget curl binutils git
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y libsqlite3-0 libsqlite3-dev build-essential jq wget curl binutils git
