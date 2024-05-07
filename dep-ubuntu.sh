c4b4a174811f18b26e4851a0910e8861db33b4ae
Dpbm <dpbm136@gmail.com>
2024-05-07 15:49:10 -0300

    fixed yq install permissions

--- dep-ubuntu.sh
+++ dep-ubuntu.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y libsqlite3-0 libsqlite3-dev build-essential jq wget curl binutils git ninja-build libgtk-3-dev
-wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
