ad745b9c6800d4ba0bdd164f477b1197fa9b546a
Dpbm <dpbm136@gmail.com>
2024-05-09 14:45:14 -0300

    added icons

---
+++ icon_config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Setting the icons up..."
+dart run flutter_launcher_icons
--- splash_config.sh
+++ splash_config.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "Setting the splash screen up..."
 dart run flutter_native_splash:create
