f7cd6be0f090e02c59959556298d5de60678642d
Dpbm <dpbm136@gmail.com>
2024-05-18 16:09:58 -0300

    new create preview method

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -7,11 +7,43 @@ class Note {
   const Note({this.id, this.preview, required this.title, required this.text});
 
   Map<String, Object?> toMap() {
-    String textFix = text.replaceAll("\n", " ");
+    const int maxChar = 40;
+    const int maxLines = 4;
+    const double lengthPerLine = maxChar / maxLines;
+
+    List<String> textLines = text.split("\n");
+    String preview = '';
+
+    if (textLines.isNotEmpty) {
+      int lineCounter = 0;
+      int totalExceded = 0;
+
+      for (String line in textLines) {
+        if (lineCounter >= maxLines) break;
+        preview += line;
+        preview += '\n';
+        lineCounter++;
+
+        if (line.length > lengthPerLine) {
+          totalExceded += line.length - lengthPerLine.toInt();
+        }
+      }
+
+      if (totalExceded > 0) {
+        preview = preview.substring(0, preview.length - totalExceded);
+        preview += '...';
+      } else if (textLines.length > 4) {
+        preview += '...';
+      }
+    } else {
+      preview =
+          text.length >= maxChar ? text.substring(0, maxChar) + '...' : text;
+    }
+
     return {
       'id': id,
       'title': title,
-      'preview': textFix.length < 40 ? textFix : (textFix.substring(0, 40) + '...'),
+      'preview': preview,
       'text': text,
       'date': DateTime.now().toString(),
       'search': title + ' ' + text
